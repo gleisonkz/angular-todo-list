4357996f364c071acb17c8f6985352ca521dcae4
Gleison de Almeida <48810597+gleisonkz@users.noreply.github.com>
2021-02-04 15:02:58 -0300

    ✨ feat: custom-error-state matcher class

--- front-end/src/app/app.module.ts
+++ front-end/src/app/app.module.ts
@@ -2,6 +2,7 @@ import { HttpClientModule } from '@angular/common/http';
 import { NgModule } from '@angular/core';
 import { ReactiveFormsModule } from '@angular/forms';
 import { MatCheckboxModule } from '@angular/material/checkbox';
+import { ErrorStateMatcher } from '@angular/material/core';
 import { MatIconModule } from '@angular/material/icon';
 import { MatInputModule } from '@angular/material/input';
 import { BrowserModule } from '@angular/platform-browser';
@@ -9,6 +10,7 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { AppComponent } from './app.component';
 import { TodoListComponent } from './components/todo-list/todo-list.component';
 import { TodoComponent } from './components/todo/todo.component';
+import { CustomErrorStateMatcher } from './custom-error-state-matcher';
 
 @NgModule({
   declarations: [AppComponent, TodoComponent, TodoListComponent],
@@ -21,7 +23,9 @@ import { TodoComponent } from './components/todo/todo.component';
     ReactiveFormsModule,
     HttpClientModule,
   ],
-  providers: [],
+  providers: [
+    { provide: ErrorStateMatcher, useValue: new CustomErrorStateMatcher() },
+  ],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
