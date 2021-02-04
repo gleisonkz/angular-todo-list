96cc34a625c13188dee550310aeb16446451e3ba
Gleison de Almeida <48810597+gleisonkz@users.noreply.github.com>
2021-02-04 15:04:06 -0300

    ✨ feat: todo-list component

--- front-end/src/app/components/todo-list/todo-list.component.ts
+++ front-end/src/app/components/todo-list/todo-list.component.ts
@@ -1,12 +1,30 @@
 import { Component, OnInit } from '@angular/core';
+import { Todo } from 'src/app/models/todo.model';
+import { UpdateEvent } from 'src/app/models/update-event';
+import { TodoResource } from './../../models/todo.model';
+import { TodoService } from './../../services/todo.service';
 
 @Component({
-  selector: 'app-todo-list',
+  selector: 'todo-list',
   templateUrl: './todo-list.component.html',
   styleUrls: ['./todo-list.component.scss'],
 })
 export class TodoListComponent implements OnInit {
-  constructor() {}
+  todos: Todo[];
 
-  ngOnInit(): void {}
+  constructor(private todoService: TodoService) {}
+
+  ngOnInit(): void {
+    this.todoService
+      .getAllEntities()
+      .subscribe((todos: Todo[]) => (this.todos = todos));
+  }
+
+  updateTodo(event: UpdateEvent<TodoResource>): void {
+    console.log(event);
+
+    this.todoService
+      .putEntity(event.itemID, event.item)
+      .subscribe((todo) => console.log(todo));
+  }
 }
